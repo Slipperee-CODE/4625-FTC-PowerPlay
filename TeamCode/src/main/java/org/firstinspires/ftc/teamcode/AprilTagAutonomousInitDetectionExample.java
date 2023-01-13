fc50d3e9b690e858813458165ed44572fed98e66
CaiScheidler <scheidlercai@gmail.com>
2023-01-13 12:00:50 -0800

    April Tags something

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AprilTagAutonomousInitDetectionExample.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AprilTagAutonomousInitDetectionExample.java
@@ -212,7 +212,7 @@ public class AprilTagAutonomousInitDetectionExample extends LinearOpMode
 
             if (tagOfInterest.id == ID_TAGS_OF_INTEREST[0]) //First Image
             {
-                Move("left",1300,0.6);
+                Move("right",1300,0.6);
                 Move("forward",250,1);
             }
             else if(tagOfInterest.id == ID_TAGS_OF_INTEREST[1]) //Second Image
@@ -221,7 +221,7 @@ public class AprilTagAutonomousInitDetectionExample extends LinearOpMode
             }
             else if (tagOfInterest.id == ID_TAGS_OF_INTEREST[2]) //Third Image
             {
-                Move("right",1300,0.6);
+                Move("left",1300,0.6);
                 Move("forward",250,1);
             }
         }
